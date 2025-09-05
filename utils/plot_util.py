a22dc11b4eafd7a1c0060d2493bbb28abc130bc1
ladnik <niklasladurner03@gmail.com>
2025-09-05 03:43:12 +0200

    Work on thesis
    Former-commit-id: adccbad95dba2e2894385144a662826155711874

--- utils/plot_util.py
+++ utils/plot_util.py
@@ -3,33 +3,37 @@
 import csv
 import os
 import re
+import sys
 
 from classes.Config import PLOT_DATA_DIR
 from classes.PlotData import PlotData
 
 
 def clean_up_files(abs_path):
-    tuning_log_pattern = re.compile(r"tuningLog.*")
-    iteration_log_pattern = re.compile(r".*iterationPerformance.*")
-    liveinfo_log_pattern = re.compile(r".*liveInfoLogger.*")
+    tuning_log_pattern = re.compile(r".*tuningLog.*Rank(\d+).*")
+    iteration_log_pattern = re.compile(r".*iterationPerformance.*Rank(\d+)*")
+    liveinfo_log_pattern = re.compile(r".*liveInfoLogger.*Rank(\d+)*")
     plot_pattern = re.compile(r".*png")
 
     skip = False
     for f in os.listdir(abs_path):
-        if tuning_log_pattern.match(f):
+        m = tuning_log_pattern.match(f)
+        if m:
             os.rename(
                 os.path.join(abs_path, f),
-                os.path.join(abs_path, "tuningLog.txt"),
+                os.path.join(abs_path, f"tuningLog_Rank{m.group(1)}.txt"),
             )
-        if iteration_log_pattern.match(f):
+        m = iteration_log_pattern.match(f)
+        if m:
             os.rename(
                 os.path.join(abs_path, f),
-                os.path.join(abs_path, "iterationLog.csv"),
+                os.path.join(abs_path, f"iterationLog_Rank{m.group(1)}.csv"),
             )
-        if liveinfo_log_pattern.match(f):
+        m = liveinfo_log_pattern.match(f)
+        if m:
             os.rename(
                 os.path.join(abs_path, f),
-                os.path.join(abs_path, "liveinfoLog.csv"),
+                os.path.join(abs_path, f"liveinfoLog_Rank{m.group(1)}.csv"),
             )
         if plot_pattern.match(f):
             skip = True
@@ -49,76 +53,145 @@ def estimate_tuning_triggers(param_name, iteration_log, factor):
                 timestamps += [i]
 
         print(
-            f"TimeBasedSimply dynamic tuning with a retuning factor of {factor} would have lead to an estimated {len(timestamps)} tuning triggers at times {timestamps}"
+            f"TimeBasedSimple dynamic tuning with a retuning factor of {factor} would have lead to an estimated {len(timestamps)} tuning triggers at times {timestamps}"
         )
 
 
 def collect_runtimes(parent_path, output_name):
     """Collects the total runtimes for all jobs in parent_path"""
 
+    iterations = {
+        "equilibrium": 150000,
+        "exploding-liquid": 150000,
+        "heating-sphere": 60000,
+    }
     data = {}
 
     for job in os.listdir(parent_path):
-        # print(f"job: {job}")
         if not os.path.isdir(os.path.join(parent_path, job)):
             continue
         log = os.path.join(os.path.join(parent_path, job), "job_log.txt")
-        # print(f"log: {log}")
         if not os.path.isfile(log):
             continue
+
+        if not "dynamic" in job and not "static" in job:
+            continue
+
+        scenario = next((s for s in iterations.keys() if s in job), "")
+        isBaseline = "static" in job
 
         with open(log, "r") as logfile:
             lines = logfile.readlines()
             line = "".join(lines[len(lines) - 26 :])
-            runtime = re.search(r"Total accumulated\s*:\s([0-9]+)", line).group(1)
-            data[job] = runtime
+            runtime = re.search(r"Total accumulated\s*:\s([0-9]+)", line)
+            runtime = -1 if runtime is None else runtime.group(1)
+            tuning_its = re.search(r"Tuning iterations\s*:\s*(\d+)", line)
+            tuning_its = -1 if tuning_its is None else tuning_its.group(1)
+            data[job] = {"runtime": runtime, "tuning_its": tuning_its}
+            if runtime == -1 or tuning_its == -1:
+                print(f"Incomplete job: {job}")
 
-    data = dict(sorted(data.items()))
-    print(data)
+    def sort_fn(k):
+        parts = k.split("_")
+        print(parts)
+        if "static" in parts:
+            return (parts[0], "", 0, float("inf"))
+        return (parts[0], parts[2], -int(parts[4]), float(parts[3]))
+
+    data = dict(sorted(data.items(), key=lambda kv: sort_fn(kv[0])))
+    # print(data)
     with open(output_name, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["job", "total_runtime"])
-        for job, runtime in data.items():
-            writer.writerow([job, runtime])
+        writer.writerow(
+            ["job", "scenario", "isBaseline", "total_runtime", "tuning_iterations"]
+        )
+        for job, d in data.items():
+            writer.writerow([job, scenario, isBaseline, d["runtime"], d["tuning_its"]])
 
 
 def main():
+    range_start = 0
+    range_end = sys.maxsize
+    if len(sys.argv) == 2:
+        all_dirs = [sys.argv[1]]
+        print(all_dirs)
+    elif len(sys.argv) == 4:
+        all_dirs = [sys.argv[1]]
+        range_start = int(sys.argv[2])
+        range_end = int(sys.argv[3])
+        print(all_dirs)
+    else:
+        all_dirs = os.listdir(os.path.join(PLOT_DATA_DIR, "output"))
 
-    all_dirs = os.listdir(os.path.join(PLOT_DATA_DIR, "output"))
+    # dir = os.path.join(PLOT_DATA_DIR, "output")
+    # collect_runtimes(dir, os.path.join(dir, "runtimes.csv"))
 
     for job_dir in all_dirs:
-        abs_path = os.path.join(os.path.join(PLOT_DATA_DIR, "output"), job_dir) + "/"
+        print(f"Working on {job_dir}")
+        out_dir_path = os.path.join(PLOT_DATA_DIR, "output") + "/"
+        abs_path = os.path.join(out_dir_path, job_dir) + "/"
+
+        if not os.path.isdir(abs_path):
+            continue
         skip = clean_up_files(abs_path)
         # if skip:
         #     continue
-        print(f"Working on {job_dir}")
 
-        iteration_log = os.path.join(abs_path, "iterationLog.csv")
-        liveinfo_log = os.path.join(abs_path, "liveinfoLog.csv")
+        ranks = (
+            ["Rank0", "Rank1", "Rank2", "Rank3"]
+            if "exploding-liquid" in job_dir
+            else ["Rank0"]
+        )
 
-        with PlotData(job_dir, liveinfo_log, iteration_log) as plot_instance:
-            plot_instance.plot_iteration_runtime(abs_path + "runtime", False, False)
+        for rank in ranks:
+            iteration_log = os.path.join(abs_path, f"iterationLog_{rank}.csv")
+            liveinfo_log = os.path.join(abs_path, f"liveinfoLog_{rank}.csv")
+            tuning_log = os.path.join(abs_path, f"tuningLog_{rank}.txt")
 
-            plot_instance.plot_iteration_runtime(
-                abs_path + "runtime_mark_tuning", False
-            )
-            plot_instance.plot_iteration_runtime(abs_path + "configs")
-            plot_instance.plot_iteration_runtime(
-                abs_path + "configs_nomark", True, False
-            )
+            # with PlotData(job_dir, liveinfo_log, iteration_log, tuning_log, 5000, 20000) as plot_instance:
+            with PlotData(
+                job_dir,
+                liveinfo_log,
+                iteration_log,
+                tuning_log,
+                rank,
+                range_start,
+                range_end,
+            ) as plot_instance:
+                # plot_instance.plot_iteration_runtime(abs_path + "runtime", mark_configs=False, mark_tuning_phases=False)
+                # plot_instance.plot_iteration_runtime(
+                #     abs_path + "runtime_mark_tuning", mark_configs=False
+                # )
 
-            plot_instance.plot_liveinfo_params(
-                [
-                    "avgParticlesPerCell",
-                    "estimatedNumNeighborInteractions",
-                    "maxDensity",
-                    "particlesPerCellStdDev",
-                    "numEmptyCells",
-                ],
-                abs_path + f"liveinfo_",
-            )
-        # estimate_tuning_triggers("computeInteractionsTotal[ns]", abs_path+ "iterationLog.csv", 45.0)
-        # collect_runtimes(os.path.join(PLOT_DATA, "output"), "runtimes.csv")
+                plot_instance.plot_iteration_runtime(abs_path + "configs")
+                plot_instance.plot_rebuild_times(abs_path + "configs_rebuild")
+                # plot_instance.plot_iteration_runtime(
+                #     abs_path + "configs_nomark", mark_tuning_phases=False
+                # )
+
+                plot_instance.plot_liveinfo_params(
+                    [
+                        # "avgParticlesPerCell",
+                        # "estimatedNumNeighborInteractions",
+                        "maxDensity",
+                        # "particlesPerCellStdDev",
+                        # "numEmptyCells",
+                    ],
+                    abs_path + f"liveinfo_",
+                    mark_configs=False
+                )
+
+                # plot_instance.write_tuning_results(abs_path + "tuning_results")
+
+                # if not "static" in job_dir:
+                #     plot_instance.compare_tuning_results(
+                #         out_dir_path
+                #         + job_dir.split("_")[0]
+                #         + "_static/tuning_results.csv",
+                #         abs_path + "tuning_histogramm",
+                #     )
+
+    # estimate_tuning_triggers("computeInteractionsTotal[ns]", abs_path+ "iterationLog.csv", 45.0)
 
 
 if __name__ == "__main__":
